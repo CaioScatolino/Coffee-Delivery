c0c7c7f7b6f2f1db19a7305e48c8e40b9c735ed7
Caio Scatolino <caio.scatolino@outlook.com>
2023-03-09 21:01:51 -0300

    Atualizando projeto

--- src/pages/Home/Catalog/index.tsx
+++ src/pages/Home/Catalog/index.tsx
@@ -1,3 +1,4 @@
+import { ShoppingCartSimple } from 'phosphor-react'
 import { useContext } from 'react'
 import { CoffeeContext } from '../../../contexts/CoffeeContext'
 import { CatalogContainer, CoffeeItem } from './style'
@@ -11,7 +12,23 @@ export function Catalog() {
       <h1>Nossos cafés</h1>
       <ul>
         {CoffeeCatalog.map((coffee) => {
-          return <CoffeeItem key={coffee.id}>{coffee.title}</CoffeeItem>
+          return (
+            <CoffeeItem key={coffee.id}>
+              <p>{coffee.image}</p>
+              <h5>
+                {coffee.tag[0]} {coffee.tag[1]} {coffee.tag[2]}
+              </h5>
+              <h4>{coffee.title}</h4>
+              <p>{coffee.description}</p>
+              <div>
+                <span>R$ {coffee.price}</span>
+                <input type="number" />
+                <button type="submit">
+                  <ShoppingCartSimple size={22} />
+                </button>
+              </div>
+            </CoffeeItem>
+          )
         })}
       </ul>
     </CatalogContainer>
--- src/pages/Home/Catalog/style.ts
+++ src/pages/Home/Catalog/style.ts
@@ -5,6 +5,21 @@ export const CatalogContainer = styled.main`
     display: flex;
     gap: 2rem;
     list-style: none;
+    flex-wrap: wrap;
+
+    li {
+      display: flex;
+      flex-direction: column;
+      gap: 6px;
+      width: 16rem;
+      align-items: center;
+      justify-content: center;
+      background: red;
+
+      h5 {
+        gap: 10px;
+      }
+    }
   }
 `
 
